e94f3d21b00c31063ea5f5e1dead30479d84d3e1
dgusfr <diegogustavo.fr@gmail.com>
2025-02-25 10:35:13 -0300

    feat add name in lists

--- src/application/ListProgram.java
+++ src/application/ListProgram.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String> list = new ArrayList<>();
+		
+		list.add("Maria");
+		list.add("João");
+		list.add("José");
+		list.add("Renata");
+		list.add("Claudio");
+		
+		for(String name : list) {
+			System.out.println(name);
+		}
+		
 	}
 
 }
